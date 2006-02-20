037bae7264197ee6436cda1649eac4e5f8befadd
tgdwyer <tgdwyer@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2006-02-20 22:07:35 +0000

    new test case

--- solve_VPSC/tests/test_block.cpp
+++ solve_VPSC/tests/test_block.cpp
@@ -21,7 +21,45 @@ void test1() {
 	cout << "  lm=" << c->lm << endl;
 	cout << "Block test 1... Success!" << endl;
 }
+
+/*
+ * Contraint tree:
+ *    \_/
+ *    / \
+ */
+void test2() {
+	Variable *a[]={
+		new Variable(0,0,1),
+		new Variable(1,0,1),
+		new Variable(2,1,1),
+		new Variable(3,2,1),
+		new Variable(4,3,1),
+		new Variable(5,3,1)};
+	Constraint *c[]={
+		new Constraint(a[0],a[2],2),
+		new Constraint(a[1],a[2],2),
+		new Constraint(a[2],a[3],2),
+		new Constraint(a[3],a[4],2),
+		new Constraint(a[3],a[5],2)};
+	for(int i=0;i<6;i++) {
+		new Block(a[i]);
+	}
+	for(int i=0;i<5;i++) {
+		Block *l=c[i]->left->block, *r=c[i]->right->block;
+		l->merge(r,c[i]);
+	}
+	Block *b=a[0]->block;
+	cout << "Block: " << *b << endl;
+	for(int i=0;i<6;i++) {
+			a[i]->desiredPosition = i<3?-2:5;
+	}
+	cout << "calc min lm:" << endl;
+	Constraint *m = b->findMinLMBetween(a[0],a[5]);
+	cout << "Min lm constraint: " << *m << endl;
+	assert(m==c[2]);
+}
 int main() {
 	test1();
+	test2();
 	return 0;
 }
