26872c94586387d9d74e515a90cf56c08a547ae7
kierans <kierans@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2006-07-11 07:46:17 +0000

    * Chenged return type of visit() to void * Fixed method def in graphlayouttest

--- cola/libcola/tests/graphlayouttest.h
+++ cola/libcola/tests/graphlayouttest.h
@@ -1,7 +1,7 @@
 #include <cola.h>
 void output_svg(vector<Rectangle*> &rs, vector<cola::Edge>& es, char* fname,bool rects=false);
 void output_svg(vector<Rectangle*> &rs, vector<straightener::Edge*>& es, char* fname,bool rects=false);
-void output_svg(vector<Rectangle*> &rs, vector<cola::Edge>& es, char* fname,bool rects=false, bool directedEdges=false, vector<bool> * cyclicEdges=NULL);
+void output_svg(vector<Rectangle*> &rs, vector<cola::Edge>& es, char* fname,bool rects, bool directedEdges, vector<bool> * cyclicEdges);
 
 inline double getRand(double range) {
 	return range*rand()/RAND_MAX;
