c431ff523229a9257e6ad310254dc7561d7601b0
kierans <kierans@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2006-07-11 07:33:12 +0000

    Added little script to view svg output of cycle detector tests

---
+++ cola/libcola/tests/view_output.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+if [ $# -lt 1 ] ; then
+  echo "Need case letter (eg: A) or ALL for all cases"
+  exit
+else
+  CASE=$1
+  shift
+fi
+
+cd /home/kierans/libcola2/tests
+ 
+if [ $CASE == 'a' -o $CASE == 'all' ] ; then
+  inkview cycle_detector_case_a.svg
+fi
+
+if [ $CASE == 'b' -o $CASE == 'all' ] ; then
+  inkview cycle_detector_case_b.svg
+fi
+
+if [ $CASE == 'c' -o $CASE == 'all' ] ; then
+  inkview cycle_detector_case_c.svg
+fi
+
+if [ $CASE == 'd' -o $CASE == 'all' ] ; then
+  inkview cycle_detector_case_d.svg
+fi
+
+if [ $CASE == 'e' -o $CASE == 'all' ] ; then
+  inkview cycle_detector_case_e.svg
+fi
+
+if [ $CASE == 'f' -o $CASE == 'all' ] ; then
+  inkview cycle_detector_case_f.svg
+fi
