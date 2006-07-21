a990a4956892ad1bfb27314ad245801fa6d1ce9e
kierans <kierans@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2006-07-21 07:56:43 +0000

    Committed Maximum Acyclic Subgraph Implementation

--- cola/libcola/tests/graphlayouttest.cpp
+++ cola/libcola/tests/graphlayouttest.cpp
@@ -189,3 +189,103 @@ void output_svg(vector<Rectangle*>& rs, vector<Edge>& es, char *fname, bool rect
 	}
 	f<<"</svg>"<<endl;
 }
+
+void output_svg(vector<Rectangle*>& rs, vector<Edge>& es, char *fname, bool rects, bool directedEdges, vector<Edge>* Ea) {
+	double width, height;
+	ofstream f(fname);
+	f.setf(ios::fixed);
+	double r=5;
+	if(rects) r=rs[0]->width()/2;
+	double xmin=DBL_MAX, ymin=xmin;
+	double xmax=-DBL_MAX, ymax=xmax;
+	for (unsigned i=0;i<rs.size();i++) {
+		double x=rs[i]->getCentreX(), y=rs[i]->getCentreY();
+		xmin=min(xmin,x);
+		ymin=min(ymin,y);
+		xmax=max(xmax,x);
+		ymax=max(ymax,y);
+	}
+	xmax+=2*r;
+	ymax+=2*r;
+	xmin-=2*r;
+	ymin-=2*r;
+	width=xmax-xmin;
+	height=ymax-ymin;
+	f << "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" ";
+	f << "xmlns:sodipodi=\"http://inkscape.sourceforge.net/DTD/sodipodi-0.dtd\" ";
+	f << "xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" width=\""<<width<<"\" height=\""<<height<<"\" viewBox = \"";
+	f << xmin<<" "<<ymin<<" "<<width<<" "<<height<<"\">"<<endl;
+
+        // code to start to draw arrow heads
+	if (directedEdges)  {
+	  f << "<defs id=\"defs4\">" << endl;
+          f << "  <marker inkscape:stockid=\"ConnArrowEnd\"" << endl;
+          f << "    orient=\"auto\" refY=\"0.0\" refX=\"" << 4.5 + r + ARROW_HEAD_BUFFER << "\"" << endl;
+          f << "    id=\"ConnArrowEnd\" style=\"overflow:visible;\">" << endl;
+          f << "  <path id=\"path3466\"" << endl;
+          f << "    d=\"M 0.0,0.0 L 1.0,-5.0 L -12.5,0.0 L 1.0,5.0 L 0.0,0.0 z \"" << endl;
+          f << "    style=\"fill-rule:evenodd;stroke:#000000;stroke-width:1.0px;marker-start:none;\"" << endl;
+          f << "    transform=\"scale(0.6) rotate(180)\" />" << endl;
+          f << "  </marker>" << endl;
+          f << "</defs>" << endl;
+	  f << "<defs id=\"defs5\">" << endl;
+          f << "  <marker inkscape:stockid=\"CyclicConnArrowEnd\"" << endl;
+          f << "    orient=\"auto\" refY=\"0.0\" refX=\"" << 4.5 + r + ARROW_HEAD_BUFFER << "\"" << endl;
+          f << "    id=\"CyclicConnArrowEnd\" style=\"overflow:visible;\">" << endl;
+          f << "  <path id=\"path3466\"" << endl;
+          f << "    d=\"M 0.0,0.0 L 1.0,-5.0 L -12.5,0.0 L 1.0,5.0 L 0.0,0.0 z \"" << endl;
+          f << "    style=\"fill-rule:evenodd;stroke:#B400FF;stroke-width:1.0px;marker-start:none;\"" << endl;
+          f << "    transform=\"scale(0.6) rotate(180)\" />" << endl;
+          f << "  </marker>" << endl;
+          f << "</defs>" << endl;
+	}
+
+	for (unsigned i=0;i<Ea->size();i++) {
+		unsigned u = (*Ea)[i].first;
+		unsigned v = (*Ea)[i].second;
+
+		if (directedEdges)  {
+		  f << "<path d=\"M" << rs[u]->getCentreX() << "," << rs[u]->getCentreY() << " L" << rs[v]->getCentreX() << "," << rs[v]->getCentreY();
+		  f << "\"";
+                  f << " style=\"stroke:rgb(99,99,99);stroke-width:1px;marker-end:url(#ConnArrowEnd)\"/>" << endl;
+		}
+		else  {
+		  f<<"<line x1=\""<<rs[u]->getCentreX()
+		   <<"\" y1=\""<<rs[u]->getCentreY()
+		   <<"\" x2=\""<<rs[v]->getCentreX()
+		   <<"\" y2=\""<<rs[v]->getCentreY()
+		   <<"\" style=\"stroke:rgb(99,99,99);stroke-width:2\"/>"<<endl;
+		}	
+	}
+	for (unsigned i=0;i<rs.size();i++) {
+		f<<"<g id=\"node"<<i
+		 <<"\" class=\"node\"><title>"<<i<<"</title>"<<endl;
+		if(!rects) {
+			f<<"<ellipse cx=\""<<rs[i]->getCentreX()
+			 <<"\" cy=\""<<rs[i]->getCentreY()
+			 <<"\" rx=\""<<r<<"\" ry=\""<<r
+			 <<"\" style=\"fill:black;stroke:black;\"/>"
+			 <<"<text x=\"";
+			
+			if (i < 10)  { f << (rs[i]->getCentreX()-2); }
+			else  { f << (rs[i]->getCentreX()-4); }
+
+			f <<"\" y=\""<<(rs[i]->getCentreY()+2)
+			  <<"\" style=\"fill:white\" font-size=\"7\">"<< i
+			  <<"</text>"<<endl;		
+		} else {
+			f<<"<rect x=\""<<rs[i]->getMinX()
+			 <<"\" y=\""<<rs[i]->getMinY()
+			 <<"\" width=\""<<rs[i]->width()
+			 <<"\" height=\""<<rs[i]->height()
+			 <<"\" style=\"fill:white;stroke:black;\"/>";
+			f<<"<text x=\""<<(rs[i]->getCentreX()-3)
+			 <<"\" y=\""<<(rs[i]->getCentreY()+5)
+			 <<"\">"<<i
+			 <<"</text>"<<endl;
+
+		}
+		f <<endl<<"</g>"<<endl;
+	}
+	f<<"</svg>"<<endl;
+}
--- cola/libcola/tests/graphlayouttest.h
+++ cola/libcola/tests/graphlayouttest.h
@@ -2,6 +2,7 @@
 void output_svg(vector<vpsc::Rectangle*> &rs, vector<cola::Edge>& es, char* fname,bool rects=false);
 void output_svg(vector<vpsc::Rectangle*> &rs, vector<straightener::Edge*>& es, char* fname,bool rects=false);
 void output_svg(vector<vpsc::Rectangle*> &rs, vector<cola::Edge>& es, char* fname,bool rects, bool directedEdges, vector<bool> * cyclicEdges);
+void output_svg(vector<vpsc::Rectangle*> &rs, vector<cola::Edge>& es, char* fname,bool rects, bool directedEdges, vector<cola::Edge>* Ea);
 
 inline double getRand(double range) {
 	return range*rand()/RAND_MAX;
