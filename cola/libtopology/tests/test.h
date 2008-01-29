a24308e2cf5fd958b9509c787448084685e53eab
tgdwyer <tgdwyer@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2008-01-29 04:38:49 +0000

    new test case

--- cola/libtopology/tests/test.h
+++ cola/libtopology/tests/test.h
@@ -1,4 +1,30 @@
 
+template <typename T>
+T getRand(T range) {
+    double d=static_cast<double>(rand())/static_cast<double>(RAND_MAX);
+    T r=static_cast<T>(static_cast<double>(range)*d);
+	return r;
+}
+
+void generateRandomRects(unsigned n, vpsc::Rectangles &rs) {
+	rs.resize(n);
+	const int rect_size = 50;
+	const int fld_size = static_cast<int>(
+            sqrt(static_cast<double>(rect_size * n) / 2.0));
+	int coords[4];
+	for (unsigned i = 0; i < n; ++i) {
+		for (unsigned d = 0; d < 2; ++d) {
+			//unsigned const end = 1 + (rand() % (fld_size - 1));
+			//unsigned const start = rand() % end;
+			int const start = getRand(fld_size);
+			int const end = start + getRand(rect_size) + 1;
+			coords[2 * d] = start;
+			coords[2 * d + 1] = end;
+		} 
+		rs[i]=new vpsc::Rectangle(coords[0],coords[1],coords[2],coords[3]);
+	}
+}
+
 Node* addNode(Nodes& vs, double x, double y, double w, double h) {
     vpsc::Rectangle* r = new vpsc::Rectangle(x,x+w,y,y+h);
     Node *v = new Node(vs.size(), r);
