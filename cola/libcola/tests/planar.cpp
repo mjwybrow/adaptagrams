4bc47df68b0942ea6f55395db5d28de606097971
mjwybrow <mjwybrow@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2008-07-11 02:47:13 +0000

    o  Add LGPL license headers to all source files.

--- cola/libcola/tests/planar.cpp
+++ cola/libcola/tests/planar.cpp
@@ -1,3 +1,28 @@
+/*
+ * vim: ts=4 sw=4 et tw=0 wm=0
+ *
+ * libcola - A library providing force-directed network layout using the 
+ *           stress-majorization method subject to separation constraints.
+ *
+ * Copyright (C) 2006-2008  Monash University
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library in the file LICENSE; if not, 
+ * write to the Free Software Foundation, Inc., 59 Temple Place, 
+ * Suite 330, Boston, MA  02111-1307  USA
+ *
+*/
+
 /** \file topology.cpp
  *
  * tests topology preserving layout.  graph is a triangle.
@@ -10,10 +35,6 @@
 /*
 * Authors:
 *   Tim Dwyer <tgdwyer@gmail.com>
-*
-* Copyright (C) 2005 Authors
-*
-* Released under GNU GPL.  Read the file 'COPYING' for more information.
 */
 #include <iostream>
 #include <iomanip>
