7959f5dac04d04a31c70121ccfa36ccd532cd90a
mjwybrow <mjwybrow@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2009-09-14 02:10:13 +0000

    -  Improve some test cases.

---
+++ cola/libavoid/tests/inlineoverlap02.cpp
@@ -0,0 +1,65 @@
+// From p-overlap bug.
+#include "libavoid/libavoid.h"
+using namespace Avoid;
+int main(void) {
+    Router *router = new Router(
+            PolyLineRouting | OrthogonalRouting);
+    router->setRoutingPenalty((PenaltyType)0, 50);
+    router->setRoutingPenalty((PenaltyType)1, 0);
+    router->setRoutingPenalty((PenaltyType)2, 0);
+    router->setRoutingPenalty((PenaltyType)3, 4000);
+    router->setRoutingPenalty((PenaltyType)4, 110);
+    router->setOrthogonalNudgeDistance(25);
+
+    Polygon poly548374163(4);
+    poly548374163.ps[0] = Point(52285, 50415);
+    poly548374163.ps[1] = Point(52285, 50885);
+    poly548374163.ps[2] = Point(51915, 50885);
+    poly548374163.ps[3] = Point(51915, 50415);
+    ShapeRef *shapeRef548374163 = new ShapeRef(router, poly548374163, 548374163);
+    router->addShape(shapeRef548374163);
+
+    Polygon poly942733064(4);
+    poly942733064.ps[0] = Point(51829, 50800);
+    poly942733064.ps[1] = Point(51829, 50885);
+    poly942733064.ps[2] = Point(51771, 50885);
+    poly942733064.ps[3] = Point(51771, 50800);
+    ShapeRef *shapeRef942733064 = new ShapeRef(router, poly942733064, 942733064);
+    router->addShape(shapeRef942733064);
+
+    Polygon poly335855988(4);
+    poly335855988.ps[0] = Point(51510, 50190);
+    poly335855988.ps[1] = Point(51510, 50885);
+    poly335855988.ps[2] = Point(51140, 50885);
+    poly335855988.ps[3] = Point(51140, 50190);
+    ShapeRef *shapeRef335855988 = new ShapeRef(router, poly335855988, 335855988);
+    router->addShape(shapeRef335855988);
+
+    ConnRef *connRef240732432 = new ConnRef(router, 240732432);
+    ConnEnd srcPt240732432(Point(51935, 50650), 4);
+    connRef240732432->setSourceEndpoint(srcPt240732432);
+    ConnEnd dstPt240732432(Point(51800, 50810), 1);
+    connRef240732432->setDestEndpoint(dstPt240732432);
+    connRef240732432->setRoutingType((ConnType)2);
+
+    ConnRef *connRef148374725 = new ConnRef(router, 148374725);
+    ConnEnd srcPt148374725(Point(51935, 50500), 4);
+    connRef148374725->setSourceEndpoint(srcPt148374725);
+    ConnEnd dstPt148374725(Point(51490, 50800), 8);
+    connRef148374725->setDestEndpoint(dstPt148374725);
+    connRef148374725->setRoutingType((ConnType)2);
+
+    ConnRef *connRef100850179 = new ConnRef(router, 100850179);
+    ConnEnd srcPt100850179(Point(51935, 50800), 4);
+    connRef100850179->setSourceEndpoint(srcPt100850179);
+    ConnEnd dstPt100850179(Point(51490, 50650), 8);
+    connRef100850179->setDestEndpoint(dstPt100850179);
+    connRef100850179->setRoutingType((ConnType)2);
+
+    router->processTransaction();
+    router->outputInstanceToSVG("test-inlineoverlap-02");
+    bool overlap = router->existsOrthogonalPathOverlap();
+    delete router;
+    return (overlap) ? 1 : 0;
+};
+
---
+++ cola/libavoid/tests/inlineoverlap03.cpp
@@ -0,0 +1,58 @@
+// From diffport-pins-overlap-reduced_libavoid-debug_4 bug.
+#include "libavoid/libavoid.h"
+using namespace Avoid;
+int main(void) {
+    Router *router = new Router(
+            PolyLineRouting | OrthogonalRouting);
+    router->setRoutingPenalty((PenaltyType)0, 50);
+    router->setRoutingPenalty((PenaltyType)1, 0);
+    router->setRoutingPenalty((PenaltyType)2, 0);
+    router->setRoutingPenalty((PenaltyType)3, 4000);
+    router->setRoutingPenalty((PenaltyType)4, 110);
+    router->setOrthogonalNudgeDistance(25);
+
+    Polygon poly421433292(4);
+    poly421433292.ps[0] = Point(51335, 50215);
+    poly421433292.ps[1] = Point(51335, 50385);
+    poly421433292.ps[2] = Point(51065, 50385);
+    poly421433292.ps[3] = Point(51065, 50215);
+    ShapeRef *shapeRef421433292 = new ShapeRef(router, poly421433292, 421433292);
+    router->addShape(shapeRef421433292);
+
+    Polygon poly174734866(4);
+    poly174734866.ps[0] = Point(51335, 50490);
+    poly174734866.ps[1] = Point(51335, 50660);
+    poly174734866.ps[2] = Point(51065, 50660);
+    poly174734866.ps[3] = Point(51065, 50490);
+    ShapeRef *shapeRef174734866 = new ShapeRef(router, poly174734866, 174734866);
+    router->addShape(shapeRef174734866);
+
+    Polygon poly564884274(4);
+    poly564884274.ps[0] = Point(50635, 50500);
+    poly564884274.ps[1] = Point(50635, 50583);
+    poly564884274.ps[2] = Point(50515, 50583);
+    poly564884274.ps[3] = Point(50515, 50500);
+    ShapeRef *shapeRef564884274 = new ShapeRef(router, poly564884274, 564884274);
+    router->addShape(shapeRef564884274);
+
+    ConnRef *connRef103176525 = new ConnRef(router, 103176525);
+    ConnEnd srcPt103176525(Point(51085, 50550), 4);
+    connRef103176525->setSourceEndpoint(srcPt103176525);
+    ConnEnd dstPt103176525(Point(50575, 50510), 1);
+    connRef103176525->setDestEndpoint(dstPt103176525);
+    connRef103176525->setRoutingType((ConnType)2);
+
+    ConnRef *connRef223495632 = new ConnRef(router, 223495632);
+    ConnEnd srcPt223495632(Point(51085, 50325), 4);
+    connRef223495632->setSourceEndpoint(srcPt223495632);
+    ConnEnd dstPt223495632(Point(50575, 50550), 14);
+    connRef223495632->setDestEndpoint(dstPt223495632);
+    connRef223495632->setRoutingType((ConnType)2);
+
+    router->processTransaction();
+    router->outputInstanceToSVG("test-inlineoverlap-03");
+    bool overlap = router->existsOrthogonalPathOverlap();
+    delete router;
+    return (overlap) ? 1 : 0;
+};
+
