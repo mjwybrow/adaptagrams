dc1f0e4289b0a96bd88635a5674cea6ae838c34b
mjwybrow <mjwybrow@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2009-09-25 20:02:57 +0000

    -  Improve doxygen documentation.

--- libavoid.h
+++ libavoid.h
@@ -26,51 +26,6 @@
 //! @brief Standard libavoid include file which includes all libavoid 
 //!        header files.
 
-
-//! @mainpage Object-avoiding orthogonal and polyline connector routing library
-//!
-//! @section info_sec libavoid Information
-//!
-//! libavoid is a cross-platform C++ library providing fast, object-avoiding 
-//! orthogonal and polyline connector routing for use in interactive diagram 
-//! editors.
-//!
-//! libavoid is part of the 
-//! <a href="http://adaptagrams.sourceforge.net/">Adaptagrams project</a>. 
-//! There are no official releases yet, though the code is stable and 
-//! available from the Adaptagrams 
-//! <a href="http://sourceforge.net/scm/?type=svn&group_id=154331">SVN 
-//! repository</a>.
-//! The API is documented using Doxygen, thus this documentation can be 
-//! obtained by running doxygen in the libavoid directory. There is also 
-//! a simple <a href="example.html">documented example</a>.
-//!
-//! libavoid is written and maintained by 
-//! <a href="http://www.csse.monash.edu.au/~mwybrow/">Michael Wybrow</a>,
-//! a member of the Adaptive Diagrams and Documents lab at Monash 
-//! University, Australia.
-//!
-//! The algorithms used for the connector routing are described in:
-//! -  M. Wybrow, K. Marriott, and P.J. Stuckey. Orthogonal connector 
-//!    routing.<br />
-//!    In Proceedings of 17th International Symposium on Graph Drawing 
-//!    (GD '09),<br /> 
-//!    To appear 2010. 
-//!    [<a href="http://www.csse.monash.edu.au/~mwybrow/papers/wybrow-gd-2009.pdf">PDF</a>]
-//! -  M. Wybrow, K. Marriott, and P.J. Stuckey. Incremental connector 
-//!    routing.<br />
-//!    In Proceedings of 13th International Symposium on Graph Drawing 
-//!    (GD '05),<br />
-//!    LNCS 3843, pages 446—457. Springer-Verlag, 2006. 
-//!    [<a href="http://dx.doi.org/10.1007/11618058_40">DOI</a>]
-//!    [<a href="http://www.csse.monash.edu.au/~mwybrow/papers/wybrow-gd-2005.pdf">PDF</a>]
-//! 
-//! libavoid is currently used in the following software:
-//! -  <a href="http://www.dunnart.org/">Dunnart</a>, a prototype research 
-//!    constraint-based diagram editor, and
-//! -  <a href="http://www.inkscape.org/">Inkscape</a>, the popular Open 
-//!    Source vector graphics editor.
-
 //! @namespace Avoid
 //! @brief The namespace used by code in the libavoid library.
 
