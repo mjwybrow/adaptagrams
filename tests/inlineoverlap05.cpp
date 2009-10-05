09a1ee5f06071d0eb8cbe34e335906443d9e3789
mjwybrow <mjwybrow@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2009-10-05 06:32:46 +0000

    -  Fix a bug with the fixedSharedPath penalty.  -  Add some regressions tests for this and for orthogonal ordering (not fixed).

---
+++ tests/inlineoverlap05.cpp
@@ -0,0 +1,73 @@
+// From Diplexer-reduced2_libavoid-debug.
+#include "libavoid/libavoid.h"
+using namespace Avoid;
+int main(void) {
+    Router *router = new Router(
+            PolyLineRouting | OrthogonalRouting);
+    router->setRoutingPenalty((PenaltyType)0, 50);
+    router->setRoutingPenalty((PenaltyType)1, 0);
+    router->setRoutingPenalty((PenaltyType)2, 0);
+    router->setRoutingPenalty((PenaltyType)3, 4000);
+    router->setRoutingPenalty((PenaltyType)4, 110);
+    router->setOrthogonalNudgeDistance(25);
+
+    Polygon poly231469760(4);
+    poly231469760.ps[0] = Point(6385, 9390);
+    poly231469760.ps[1] = Point(6385, 9560);
+    poly231469760.ps[2] = Point(6200, 9560);
+    poly231469760.ps[3] = Point(6200, 9390);
+    ShapeRef *shapeRef231469760 = new ShapeRef(router, poly231469760, 231469760);
+    router->addShape(shapeRef231469760);
+
+    Polygon poly7136376(4);
+    poly7136376.ps[0] = Point(6560, 9290);
+    poly7136376.ps[1] = Point(6560, 9460);
+    poly7136376.ps[2] = Point(6375, 9460);
+    poly7136376.ps[3] = Point(6375, 9290);
+    ShapeRef *shapeRef7136376 = new ShapeRef(router, poly7136376, 7136376);
+    router->addShape(shapeRef7136376);
+
+    Polygon poly215821749(4);
+    poly215821749.ps[0] = Point(6510, 9540);
+    poly215821749.ps[1] = Point(6510, 9710);
+    poly215821749.ps[2] = Point(6325, 9710);
+    poly215821749.ps[3] = Point(6325, 9540);
+    ShapeRef *shapeRef215821749 = new ShapeRef(router, poly215821749, 215821749);
+    router->addShape(shapeRef215821749);
+
+    Polygon poly83086994(4);
+    poly83086994.ps[0] = Point(5925, 9240);
+    poly83086994.ps[1] = Point(5925, 10085);
+    poly83086994.ps[2] = Point(5575, 10085);
+    poly83086994.ps[3] = Point(5575, 9240);
+    ShapeRef *shapeRef83086994 = new ShapeRef(router, poly83086994, 83086994);
+    router->addShape(shapeRef83086994);
+
+    ConnRef *connRef292408794 = new ConnRef(router, 292408794);
+    ConnEnd srcPt292408794(Point(5915, 9625), 8);
+    connRef292408794->setSourceEndpoint(srcPt292408794);
+    ConnEnd dstPt292408794(Point(6210, 9475), 4);
+    connRef292408794->setDestEndpoint(dstPt292408794);
+    connRef292408794->setRoutingType((ConnType)2);
+
+    ConnRef *connRef40917776 = new ConnRef(router, 40917776);
+    ConnEnd srcPt40917776(Point(5915, 9550), 8);
+    connRef40917776->setSourceEndpoint(srcPt40917776);
+    ConnEnd dstPt40917776(Point(6385, 9375), 4);
+    connRef40917776->setDestEndpoint(dstPt40917776);
+    connRef40917776->setRoutingType((ConnType)2);
+
+    ConnRef *connRef629564142 = new ConnRef(router, 629564142);
+    ConnEnd srcPt629564142(Point(5915, 9475), 8);
+    connRef629564142->setSourceEndpoint(srcPt629564142);
+    ConnEnd dstPt629564142(Point(6335, 9625), 4);
+    connRef629564142->setDestEndpoint(dstPt629564142);
+    connRef629564142->setRoutingType((ConnType)2);
+
+    router->processTransaction();
+    router->outputInstanceToSVG("test-inlineoverlap-05");
+    bool overlap = router->existsOrthogonalPathOverlap();
+    delete router;
+    return (overlap) ? 1 : 0;
+};
+
---
+++ tests/orthordering01.cpp
@@ -0,0 +1,92 @@
+// From bandgap_1_nooverlap_reduced_libavoid-debug.
+#include "libavoid/libavoid.h"
+using namespace Avoid;
+int main(void) {
+    Router *router = new Router(
+            PolyLineRouting | OrthogonalRouting);
+    router->setRoutingPenalty((PenaltyType)0, 50);
+    router->setRoutingPenalty((PenaltyType)1, 0);
+    router->setRoutingPenalty((PenaltyType)2, 0);
+    router->setRoutingPenalty((PenaltyType)3, 4000);
+    router->setRoutingPenalty((PenaltyType)4, 110);
+    router->setOrthogonalNudgeDistance(25);
+
+    Polygon poly143407352(4);
+    poly143407352.ps[0] = Point(5810, 4340);
+    poly143407352.ps[1] = Point(5810, 5035);
+    poly143407352.ps[2] = Point(5450, 5035);
+    poly143407352.ps[3] = Point(5450, 4340);
+    ShapeRef *shapeRef143407352 = new ShapeRef(router, poly143407352, 143407352);
+    router->addShape(shapeRef143407352);
+
+    Polygon poly124950386(4);
+    poly124950386.ps[0] = Point(4900, 4090);
+    poly124950386.ps[1] = Point(4900, 4785);
+    poly124950386.ps[2] = Point(4540, 4785);
+    poly124950386.ps[3] = Point(4540, 4090);
+    ShapeRef *shapeRef124950386 = new ShapeRef(router, poly124950386, 124950386);
+    router->addShape(shapeRef124950386);
+
+    ConnRef *connRef373967044 = new ConnRef(router, 373967044);
+    ConnEnd srcPt373967044(Point(4890, 4250), 8);
+    connRef373967044->setSourceEndpoint(srcPt373967044);
+    ConnEnd dstPt373967044(Point(5460, 4500), 4);
+    connRef373967044->setDestEndpoint(dstPt373967044);
+    connRef373967044->setRoutingType((ConnType)2);
+
+    ConnRef *connRef681881486 = new ConnRef(router, 681881486);
+    ConnEnd srcPt681881486(Point(4890, 4325), 8);
+    connRef681881486->setSourceEndpoint(srcPt681881486);
+    ConnEnd dstPt681881486(Point(5460, 4575), 4);
+    connRef681881486->setDestEndpoint(dstPt681881486);
+    connRef681881486->setRoutingType((ConnType)2);
+
+    ConnRef *connRef829752 = new ConnRef(router, 829752);
+    ConnEnd srcPt829752(Point(4890, 4400), 8);
+    connRef829752->setSourceEndpoint(srcPt829752);
+    ConnEnd dstPt829752(Point(5460, 4650), 4);
+    connRef829752->setDestEndpoint(dstPt829752);
+    connRef829752->setRoutingType((ConnType)2);
+
+    ConnRef *connRef17625739 = new ConnRef(router, 17625739);
+    ConnEnd srcPt17625739(Point(4890, 4475), 8);
+    connRef17625739->setSourceEndpoint(srcPt17625739);
+    ConnEnd dstPt17625739(Point(5460, 4725), 4);
+    connRef17625739->setDestEndpoint(dstPt17625739);
+    connRef17625739->setRoutingType((ConnType)2);
+
+    ConnRef *connRef223864175 = new ConnRef(router, 223864175);
+    ConnEnd srcPt223864175(Point(4890, 4550), 8);
+    connRef223864175->setSourceEndpoint(srcPt223864175);
+    ConnEnd dstPt223864175(Point(5460, 4800), 4);
+    connRef223864175->setDestEndpoint(dstPt223864175);
+    connRef223864175->setRoutingType((ConnType)2);
+
+    ConnRef *connRef98144280 = new ConnRef(router, 98144280);
+    ConnEnd srcPt98144280(Point(4890, 4625), 8);
+    connRef98144280->setSourceEndpoint(srcPt98144280);
+    ConnEnd dstPt98144280(Point(5460, 4875), 4);
+    connRef98144280->setDestEndpoint(dstPt98144280);
+    connRef98144280->setRoutingType((ConnType)2);
+
+    ConnRef *connRef283100856 = new ConnRef(router, 283100856);
+    ConnEnd srcPt283100856(Point(4890, 4700), 8);
+    connRef283100856->setSourceEndpoint(srcPt283100856);
+    ConnEnd dstPt283100856(Point(5460, 4950), 4);
+    connRef283100856->setDestEndpoint(dstPt283100856);
+    connRef283100856->setRoutingType((ConnType)2);
+
+    ConnRef *connRef387080925 = new ConnRef(router, 387080925);
+    ConnEnd srcPt387080925(Point(5460, 4425), 4);
+    connRef387080925->setSourceEndpoint(srcPt387080925);
+    ConnEnd dstPt387080925(Point(4890, 4175), 8);
+    connRef387080925->setDestEndpoint(dstPt387080925);
+    connRef387080925->setRoutingType((ConnType)2);
+
+    router->processTransaction();
+    router->outputInstanceToSVG("test-orthordering-01");
+    int crossings = router->existsOrthogonalCrossings();
+    delete router;
+    return (crossings > 0) ? 1 : 0;
+};
+
