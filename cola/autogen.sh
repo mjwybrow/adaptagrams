b03327974517db1979a38807166c1a4aa4b647dd
mjwybrow <mjwybrow@f338a0f1-a90d-0410-940e-aeae9343ff1a>
2009-10-07 03:20:17 +0000

    -  autogen.sh improvements.  -  Fix automake warnings.

--- cola/autogen.sh
+++ cola/autogen.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -x
+#
 # We use gnu automake to build.  I've tried to make the contents of the
 # repository as platform agnostic as possible, so you'll need to call
 # aclocal, autoconf, and automake before configure.
@@ -12,8 +14,7 @@ automake -a --add-missing
 # ./configure CPPFLAGS="-DUSE_ASSERT_EXCEPTIONS"
 ./configure
 make
-# The following builds examples in libvpsc/tests and libcola/tests.
+# The following builds examples and tests for each of the libraries.
 # Please look at these before sending me questions.
 make check
 
-# Tim Dwyer - 16/03/2007 (www.csse.monash.edu.au/~tdwyer)
