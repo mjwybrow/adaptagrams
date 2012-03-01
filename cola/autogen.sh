3d3baa5d41c390317c2387089952bf38f73da255
Michael Wybrow <mjwybrow@samediff.net>
2012-03-02 09:48:57 +1100

    Create m4 directory before calling autoreconf.

--- cola/autogen.sh
+++ cola/autogen.sh
@@ -3,20 +3,27 @@
 # We use gnu automake to build.  I've tried to make the contents of the
 # repository as platform agnostic as possible, so you'll need to call
 # autoreconf before configure.
-#
+
+# Make m4 directory, otherwise autoreconf fails when it doesn't exist.
+mkdir m4
+
 autoreconf --install --verbose
+
+# The following sequence is are the manual steps if autoreconf isn't available.
 #
-# The following sequence builds from scratch (autoreconf handles this)
 #aclocal $ACLOCAL_FLAGS
 #glibtoolize
 #autoheader
 #autoconf
 #automake -a --add-missing
-#
+
+
 # Configure.  Use the commented out line instead if building for SWIG:
 # ./configure CPPFLAGS="-DUSE_ASSERT_EXCEPTIONS"
 ./configure
+
 make
+
 # The following builds examples and tests for each of the libraries.
 # Please look at these before sending me questions.
 make check
