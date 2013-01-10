2a5cf1335c0693d983cf2db65737a31413a6e40e
Michael Wybrow <mjwybrow@samediff.net>
2013-01-10 22:34:32 +1100

    Fix a compile error introduced in previous commit.

--- cola/libtopology/orthogonal_topology.cpp
+++ cola/libtopology/orthogonal_topology.cpp
@@ -735,7 +735,7 @@ static void processLayoutConstraintEvent(LayoutScanlineNodeSet& scanline,
                 if (CONSTRAIN_CHECKPOINTS && les->containsCheckpoint && 
                         beforeLes->containsCheckpoint)
                 {
-                    // Both segments are passing a checkpoint.a
+                    // Both segments are passing a checkpoint.
                     // Constrain them together.
                     double sepDist =  
                             (les->lowPoint()[les->dimension] - 
@@ -1012,7 +1012,7 @@ static void buildOrthogonalLayoutSegments(Router *router,
             continue;
         }
         Polygon& displayRoute = (*curr)->displayRoute();
-        bool routeHasCheckpointInfo = !displayRoute.segmentHasCheckpoint.empty();
+        bool routeHasCheckpointInfo = !displayRoute.checkpointsOnRoute.empty();
         // Determine all line segments that we are interested in shifting. 
         // We don't consider the first or last segment of a path.
         for (size_t i = 1; i < displayRoute.size(); ++i)
@@ -1020,7 +1020,8 @@ static void buildOrthogonalLayoutSegments(Router *router,
             bool containsCheckpoint = false;
             if (preserveCheckpointBends && routeHasCheckpointInfo)
             {
-                containsCheckpoint = displayRoute.segmentHasCheckpoint[i - 1];
+                containsCheckpoint = 
+                        (displayRoute.checkpointOnSegment(i - 1).size() > 0);
             }
             
             if (displayRoute.ps[i - 1][dim] == displayRoute.ps[i][dim])
