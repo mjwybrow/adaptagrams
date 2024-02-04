be448d9ef1adad6a510fa7026434186b1c2cb16f
Thomas <thomas.burg@gmail.com>
2024-02-05 00:19:15 +0100

    Optimization : just what we need

--- cola/libdialect/util.h
+++ cola/libdialect/util.h
@@ -94,7 +94,7 @@ struct Matrix2d {
     std::string toString() const {
         std::string s = "";
         s += "\n  ";
-        char buffer [16];
+        char buffer [11];
         for (int j=0; j<cols; j++) {
             sprintf(buffer," %2d",j);
             s += std::string(buffer);
